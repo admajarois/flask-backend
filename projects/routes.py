e6c6cc08563e6e92beec761023459c6eced06267
AdmajaRois <rois.admj@gmail.com>
2021-09-25 21:04:08 +0700

    add styling

--- projects/routes.py
+++ projects/routes.py
@@ -1,8 +1,14 @@
+from projects.forms import LoginForm
 from flask import render_template
 from projects import app
+from projects.forms import LoginForm
 
 @app.route('/')
 @app.route('/home')
 def home():
-    
     return render_template('home.html', title="Home")
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In')
