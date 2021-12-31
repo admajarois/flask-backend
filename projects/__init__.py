aa4f2f6672e87ef9b7fd45962c1eead39ac143a0
admajarois <rois.admj@gmail.com>
2021-12-31 21:51:44 +0700

    database connection

--- projects/__init__.py
+++ projects/__init__.py
@@ -1,7 +1,7 @@
+from projects import routes
 from flask import Flask
 from projects.config import Config
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
-
-from projects import routes
--- projects/routes.py
+++ projects/routes.py
@@ -3,17 +3,19 @@ from flask import render_template
 from projects import app
 from projects.forms import LoginForm
 
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title="Home")
+
 
 @app.route('/login')
 def login():
     form = LoginForm()
     return render_template('login.html', title='Sign In')
 
+
 @app.route('/users')
 def users():
-
     return render_template('users.html')
